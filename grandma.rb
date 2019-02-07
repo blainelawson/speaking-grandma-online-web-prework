3467707e98a91b59dd583b954eaa3fa15dbc66f3
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-07 20:34:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.capitalize
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 end
