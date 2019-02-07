275752ec434fae858c379c13fd8a246fafc40aea
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-07 20:33:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,8 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase.capitalize
     "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.downcase
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
